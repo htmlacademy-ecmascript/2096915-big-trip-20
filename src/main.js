5d75022555c51917994f635d26fdd1c7fc47b46a
Vitaliy Pancyrev <vpancyrev99@gmail.com>
2023-04-30 19:49:08 +0300

    feat: render filters

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import TripInfoView from './view/trip-info-view.js';
+import FilterView from './view/filter-view.js';
 import { RenderPosition, render } from './render.js';
 
 const siteTripMainElement = document.querySelector('.trip-main');
+const siteFiltersMainElement = document.querySelector('.trip-controls');
 
 render(new TripInfoView(), siteTripMainElement, RenderPosition.AFTERBEGIN);
+render(new FilterView(), siteFiltersMainElement);
