dd39ab7d154aae039cc73e8c66a31ccb3d222a38
Vitaliy Pancyrev <vpancyrev99@gmail.com>
2023-04-30 19:53:18 +0300

    feat: render sort

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import TripInfoView from './view/trip-info-view.js';
 import FilterView from './view/filter-view.js';
+import SortView from './view/sort-view.js';
 import { RenderPosition, render } from './render.js';
 
 const siteTripMainElement = document.querySelector('.trip-main');
 const siteFiltersMainElement = document.querySelector('.trip-controls');
+const siteEventsMainElement = document.querySelector('.trip-events');
 
 render(new TripInfoView(), siteTripMainElement, RenderPosition.AFTERBEGIN);
 render(new FilterView(), siteFiltersMainElement);
+render(new SortView(), siteEventsMainElement);
