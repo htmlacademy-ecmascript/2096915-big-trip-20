4168fb6c16a7a56e9de176f28aa8a429deb27d8f
Vitaliy Pancyrev <vpancyrev99@gmail.com>
2023-04-30 21:35:06 +0300

    feat: render event edit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import TripInfoView from './view/trip-info-view.js';
 import FilterView from './view/filter-view.js';
 import SortView from './view/sort-view.js';
 import TripEventListView from './view/trip-event-list-view.js';
+import EventEditView from './view/event-edit.js';
 import { RenderPosition, render } from './render.js';
 
 const siteTripMainElement = document.querySelector('.trip-main');
@@ -12,3 +13,7 @@ render(new TripInfoView(), siteTripMainElement, RenderPosition.AFTERBEGIN);
 render(new FilterView(), siteFiltersMainElement);
 render(new SortView(), siteEventsMainElement);
 render(new TripEventListView(), siteEventsMainElement);
+
+const tripEventsList = document.querySelector('.trip-events__list');
+
+render(new EventEditView(), tripEventsList);
