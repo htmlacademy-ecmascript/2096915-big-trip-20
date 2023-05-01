f2d0f9ab183beb201d1fbcc4d3834cca0817be40
Vitaliy Pancyrev <vpancyrev99@gmail.com>
2023-05-01 15:02:44 +0300

    feat: create event presenter and change main

--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
 import TripInfoView from './view/trip-info-view.js';
 import FilterView from './view/filter-view.js';
 import SortView from './view/sort-view.js';
-import TripEventListView from './view/trip-event-list-view.js';
-import EventEditView from './view/event-edit-view.js';
-import EventEditHeaderView from './view/event-edit-header-view.js';
-import EventEditDetailsView from './view/event-edit-details-view.js';
-import TripPointView from './view/trip-point-view.js';
 import { RenderPosition, render } from './render.js';
+import EventPresenter from './presenter/event-presenter.js';
 
 const siteTripMainElement = document.querySelector('.trip-main');
 const siteFiltersMainElement = document.querySelector('.trip-controls');
@@ -15,17 +11,7 @@ const siteEventsMainElement = document.querySelector('.trip-events');
 render(new TripInfoView(), siteTripMainElement, RenderPosition.AFTERBEGIN);
 render(new FilterView(), siteFiltersMainElement);
 render(new SortView(), siteEventsMainElement);
-render(new TripEventListView(), siteEventsMainElement);
 
-const tripEventsList = document.querySelector('.trip-events__list');
+const eventPresenter = new EventPresenter({eventContainer: siteEventsMainElement});
 
-render(new EventEditView(), tripEventsList);
-
-const eventEditElement = document.querySelector('.event--edit');
-
-render(new EventEditHeaderView(), eventEditElement);
-render(new EventEditDetailsView(), eventEditElement);
-
-for (let i = 0; i < 3; i++) {
-  render(new TripPointView(), tripEventsList);
-}
+eventPresenter.init();
---
+++ src/presenter/event-presenter.js
@@ -0,0 +1,26 @@
+import TripEventListView from '../view/trip-event-list-view.js';
+import EventEditView from '../view/event-edit-view.js';
+import EventEditHeaderView from '../view/event-edit-header-view.js';
+import EventEditDetailsView from '../view/event-edit-details-view.js';
+import TripPointView from '../view/trip-point-view.js';
+import { render } from '../render.js';
+
+export default class EventPresenter {
+  tripEventListComponent = new TripEventListView();
+  eventEditComponent = new EventEditView();
+
+  constructor({eventContainer}) {
+    this.eventContainer = eventContainer;
+  }
+
+  init() {
+    render(this.tripEventListComponent, this.eventContainer);
+    render(this.eventEditComponent, this.tripEventListComponent.getElement());
+    render(new EventEditHeaderView(), this.eventEditComponent.getElement());
+    render(new EventEditDetailsView(), this.eventEditComponent.getElement());
+
+    for (let i = 0; i < 3; i++) {
+      render(new TripPointView(), this.tripEventListComponent.getElement());
+    }
+  }
+}
