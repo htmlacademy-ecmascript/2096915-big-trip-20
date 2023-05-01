62fa944830b95ccc73aebce9ec3c7a7b507f3075
Vitaliy Pancyrev <vpancyrev99@gmail.com>
2023-05-01 15:24:00 +0300

    fix: add edit form to list

--- src/presenter/event-presenter.js
+++ src/presenter/event-presenter.js
@@ -1,4 +1,5 @@
 import TripEventListView from '../view/trip-event-list-view.js';
+import TripEventItemView from '../view/trip-event-item-view.js';
 import FormEventEditView from '../view/form-event-edit-view.js';
 import EventEditHeaderView from '../view/event-edit-header-view.js';
 import EventEditDetailsView from '../view/event-edit-details-view.js';
@@ -7,6 +8,7 @@ import { render } from '../render.js';
 
 export default class EventPresenter {
   tripEventListComponent = new TripEventListView();
+  tripEventItemComponent = new TripEventItemView();
   eventEditComponent = new FormEventEditView();
 
   constructor({eventContainer}) {
@@ -15,7 +17,8 @@ export default class EventPresenter {
 
   init() {
     render(this.tripEventListComponent, this.eventContainer);
-    render(this.eventEditComponent, this.tripEventListComponent.getElement());
+    render(this.tripEventItemComponent, this.tripEventListComponent.getElement());
+    render(this.eventEditComponent, this.tripEventItemComponent.getElement());
     render(new EventEditHeaderView(), this.eventEditComponent.getElement());
     render(new EventEditDetailsView(), this.eventEditComponent.getElement());
 
