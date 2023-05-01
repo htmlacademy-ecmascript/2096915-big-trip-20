10a3075ffdda6ac03c89e0d717f9fcc0f353eb1e
Vitaliy Pancyrev <vpancyrev99@gmail.com>
2023-05-01 15:26:44 +0300

    fix: rename property

--- src/presenter/event-presenter.js
+++ src/presenter/event-presenter.js
@@ -9,7 +9,7 @@ import { render } from '../render.js';
 export default class EventPresenter {
   tripEventListComponent = new TripEventListView();
   tripEventItemComponent = new TripEventItemView();
-  eventEditComponent = new FormEventEditView();
+  formEventEditComponent = new FormEventEditView();
 
   constructor({eventContainer}) {
     this.eventContainer = eventContainer;
@@ -18,9 +18,9 @@ export default class EventPresenter {
   init() {
     render(this.tripEventListComponent, this.eventContainer);
     render(this.tripEventItemComponent, this.tripEventListComponent.getElement());
-    render(this.eventEditComponent, this.tripEventItemComponent.getElement());
-    render(new EventEditHeaderView(), this.eventEditComponent.getElement());
-    render(new EventEditDetailsView(), this.eventEditComponent.getElement());
+    render(this.formEventEditComponent, this.tripEventItemComponent.getElement());
+    render(new EventEditHeaderView(), this.formEventEditComponent.getElement());
+    render(new EventEditDetailsView(), this.formEventEditComponent.getElement());
 
     for (let i = 0; i < 3; i++) {
       render(new TripPointView(), this.tripEventListComponent.getElement());
