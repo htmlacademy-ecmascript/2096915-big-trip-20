3254d7916c231d2c3f2785448337a3b4b1665d06
Vitaliy Pancyrev <vpancyrev99@gmail.com>
2023-05-16 14:47:54 +0300

    feat: Подключает модель

--- src/mock/point.js
+++ src/mock/point.js
@@ -1,18 +1,18 @@
-import { BasePrice } from '../const.js';
-import { createIdGenerator, getRandomInteger } from '../utils.js';
+import { BasePrice, CITIES, TYPES } from '../const.js';
+import { getRandomArrayElement, createIdGenerator, getRandomInteger } from '../utils.js';
 
 const pointId = createIdGenerator();
 
-function generatePoint (type, destinationId, offerId) {
+function generatePoint () {
   return {
     id: pointId(),
     basePrice: getRandomInteger(BasePrice.MIN, BasePrice.MAX),
     dateFrom: '2019-07-10T22:55:56.845Z',
     dateTo: '2019-07-11T11:22:13.375Z',
-    destination: destinationId,
+    destination: getRandomArrayElement(CITIES),
     isFavorite: false,
-    offers: [offerId],
-    type
+    offers: [],
+    type: getRandomArrayElement(TYPES)
   };
 }
 
